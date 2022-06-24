63bcdcf0370e5eee178943bb202ad1ad7f4eafa3
uildanila <nilawildanl19@gmail.com>
2022-06-24 17:30:14 +0700

    upload file

---
+++ frontend/app.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, render_template
+
+dashboardapp = Blueprint('dashboardapp', __name__)
+
+@dashboardapp.route('/home')
+def home():
+    return render_template("index.html")
+
+@dashboardapp.route('/rfm-segmentation')
+def rfm():
+    return render_template("rfm.html")
+
+@dashboardapp.route('/product-recommendation')
+def recsys():
+    return render_template("recsys.html")
+
